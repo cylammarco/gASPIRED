00edfe504c242de5eab30f6973eb0e60849899aa
cylammarco <cylammarco@gmail.com>
2019-06-26 13:37:42 +0100

    science and standard displays are working independently now

--- src/apExtract.py
+++ src/apExtract.py
@@ -5,8 +5,16 @@ import json
 import numpy as np
 from ASPIRED import twodspec
 
-Saxis = int(float(sys.argv[1]))
-optimal = sys.argv[2]
+apwidth = int(float(sys.argv[1]))
+Saxis = int(float(sys.argv[2]))
+skysep = int(float(sys.argv[3]))
+skywidth = int(float(sys.argv[4]))
+skydeg = int(float(sys.argv[5]))
+optimal = sys.argv[6]
+cr_sigma = float(sys.argv[7])
+gain = float(sys.argv[8])
+rn = float(sys.argv[9])
+
 lines = sys.stdin.readlines()[0]
 data = json.loads(lines)
 
@@ -25,17 +33,21 @@ img[np.isnan(img)] = 0.
 
 # extract spectrum
 if optimal == 'optimal':
-	optimal = True
+    optimal = True
 else:
-	optimal = False
-spec, sky, err = twodspec.ap_extract(img, mu, trace_sigma=sigma, Saxis=Saxis, optimal=optimal)
+    optimal = False
+spec, sky, err = twodspec.ap_extract(
+	img, mu, apwidth=apwidth, trace_sigma=sigma, Saxis=Saxis, skysep=skysep,
+	skywidth=skywidth, skydeg=skydeg, optimal=optimal, cr_sigma=cr_sigma,
+	gain=gain, rn=rn, silence=True, display=False
+	)
 
 # format into a json
 json.encoder.FLOAT_REPR = lambda o: format(o, '.3f')
 if Saxis == 1:
-	my_json = json.dumps([{i: {"spec": spec[i], "sky": sky[i], "err": err[i]}} for i in range(x_len)])
+    my_json = json.dumps([{i: {"spec": spec[i], "sky": sky[i], "err": err[i]}} for i in range(x_len)])
 else:
-	my_json = json.dumps([{i: {"spec": spec[i], "sky": sky[i], "err": err[i]}} for i in range(y_len)])
+    my_json = json.dumps([{i: {"spec": spec[i], "sky": sky[i], "err": err[i]}} for i in range(y_len)])
 
 # stdout
 print(my_json)
